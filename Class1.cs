b4742bd1f9993705f3816b36f586b922907230af
Матфей <57806345+S7yka4@users.noreply.github.com>
2021-12-31 13:20:46 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    struct TypeOfTransmission
+    {
+        //код описывающий тип коробки передач 
+    }
+    abstract class Car//абстрактный класс машины содержит общие поля
+    {
+        public double VolumeOfEngine;
+        public double Power;
+    }
+
+    class PassengerCar:Car//легковая машина
+    {
+        public TypeOfTransmission Type_Of_Transmission;
+
+    }
+
+    class CargoCar:Car//грузовая машина
+    {
+        public double LiftingCapacity;
+    }
+}
